16ac83d1f3cb2a777eff4a856f1341c401a37e2c
(Student) - Patrick Darcy <C00226157@ITCARLOW.IE>
2018-11-26 11:02:29 +0000

    Intialise

---
+++ SFMLOpenGL/Game.cpp
@@ -0,0 +1,58 @@
+#include "Game.h"
+
+Game::Game() : window(VideoMode(800, 600), "OpenGL")
+{
+}
+
+Game::~Game() {}
+
+void Game::run()
+{
+
+	initialize();
+
+	Event event;
+
+	while (isRunning) {
+
+		cout << "Game running..." << endl;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				isRunning = false;
+			}
+		}
+		update();
+		draw();
+	}
+
+}
+
+void Game::initialize()
+{
+	isRunning = true;
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0); 
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void Game::update()
+{
+	cout << "Update up" << endl;
+}
+
+void Game::draw()
+{
+	cout << "Draw up" << endl;
+}
+
+void Game::unload()
+{
+	cout << "Cleaning up" << endl;
+}
+
